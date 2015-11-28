47a3c476c1c0b9a83a95252e0fad7b15769a94ff
Dong Duong <dhdong1510@gmail.com>
2015-11-28 23:07:23 +0700

    Add project spring-producing-soap-ws

---
+++ spring-producing-soap-ws/src/main/java/com/dongduong/soap/spring/CountryEndpoint.java
@@ -0,0 +1,31 @@
+package com.dongduong.soap.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.dongduong.soap.spring.service.GetCountryRequest;
+import com.dongduong.soap.spring.service.GetCountryResponse;
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://dongduong.com/soap/spring/service";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+}
