5cb1c10e676ff5c937ddbf3c1ed0fa26c295bbe9
Dong Duong <dhdong1510@gmail.com>
2016-01-07 00:42:40 +0700

    add servlet example

--- spring-producing-soap-ws/src/main/java/com/dongduong/soap/spring/Application.java
+++ spring-producing-soap-ws/src/main/java/com/dongduong/soap/spring/Application.java
@@ -2,11 +2,26 @@ package com.dongduong.soap.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ServletRegistrationBean;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+    
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(){
+        return new ServletRegistrationBean(new FooServlet(),"/servlet/*");
+    }
+
 }
